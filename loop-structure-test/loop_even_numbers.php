5e5515c0fc7724e94c24d2c72af77201f95b9f83
crymydyne <146653189+crymydyne@users.noreply.github.com>
2023-10-02 07:17:21 -0300

    logic

--- loop-structure-test/loop_even_numbers.php
+++
@@ -1,9 +0,0 @@
-<?php
-// Loop through numbers from 1 to 20
-for ($i = 1; $i <= 20; $i++) {
-    // Check if the current number is even
-    if ($i % 2 == 0) {
-        echo $i . " "; // Display the even number
-    }
-}
-?>
